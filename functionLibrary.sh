fb96645c7745381f934890c4d190a7e7c3196e54
SteveSquared14 <89590079+SteveSquared14@users.noreply.github.com>
2022-06-27 14:08:11 +0100

    Add files via upload
    Initial functions for reconTool - First iteration of following completed as of 27/6/22:
    Google Maps
    Robots.txt
    Whois - Needs work on optional name-server enumeration once that function is written

---
+++ functionLibrary.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#Function for each module of the tool
+
+#whois - takes a domain and returns any name servers
+whoIsFunc(){
+	domain=$1
+	whois $domain > tempFile.txt
+
+	#Part below here is for optional/further enumeration of named servers
+	echo "Would you like to conduction optional name-server enumeration? (Y/N)"
+	read response
+	if [[ "$response" == "Y" ]]; then
+		echo "Placeholder for name-server enumeration"
+	elif [[ "$response" == "N" ]]; then
+		cat tempFile.txt
+		rm tempFile.txt
+	fi
+}
+
+#robots.txt
+robotsTxt(){
+	urlPrepend="https://"
+	domainToOpen=$1
+	urlAppend="/robots.txt/"
+	completeURL=$urlPrepend$domainToOpen$urlAppend
+	xdg-open $completeURL
+}
+
+#exploitDB
+
+
+#google dorking
+
+
+#DNS enumeration
+#Commands to use - dig, host, nslookup, 
+#dnsCheck(){
+#	
+#}
+
+#google maps
+googleMaps(){
+	urlPrepend="https://google.com/maps/place/"
+	placeToOpen=""
+	for var in "$@"; do
+		if [[ $placeToOpen -eq "" ]]
+		then
+			placeToOpen=$var
+		else
+			placeToOpen=$placeToOpen+$var
+			shift
+		fi
+	done
+	urlAppend="/"
+	completeUrl=$urlPrepend$placeToOpen$urlAppend
+	xdg-open $completeUrl
+}
+
+
+#facebook/social media
+
+
+#metadata extraction
+
+
+#shodan
+
+
+
+#Testing below here
+whoIsFunc $1
+#googleMaps $@
+#robotsTxt $1
