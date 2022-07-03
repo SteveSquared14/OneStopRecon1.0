adc3194791b68998e0580223eccef625de19a35b
Mohammed  Choglay <72378816+CTSFO-CRYPTOCHOGLAY@users.noreply.github.com>
2022-07-03 22:14:03 +0100

    Add files via upload

--- MetadataExtractionAdaption.sh
+++ MetadataExtractionAdaption.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-file_name=$1
-file_ext="$(echo "$file_name" | grep -Eo "[.][a-z]+")"
-echo $file_ext
-
 metadata_Extraction(){
-echo  "-------------------------------------------------------------------------------Metadata Extracted ------------------------------------------------------------------------------"
-echo "File name & description:" "$(file $file_name)"
-echo "SHA512 hash:" "$(sha512sum  $file_name)"
-echo "Hex dump:" "$(hexdump $file_name | head)"
+for file in "$@"; do 
+        if [ -f $file ];
+        then
+                file_name=$1
+                file_ext="$(echo "$file_name" | grep -Eo "[.][a-z]+")"
+                echo "file extension is "$file_ext""
 
+                echo "<------------------------------------------------------------------------------- Metadata Extraction for "$file_name" ------------------------------------------------>"
+                echo "File Name & description:" "$(file $file_name)"
+                echo " "
+                echo "SHA512 Hash:" "$(sha512sum  $file_name)"
+                echo " "
+                echo "Head of Hex Dump:" "$(hexdump $file_name | head)"
+                echo " "
 
-for file in "$file_name"; do 
-        if [ -f $file_name ];
-        then 
-                if [ $file_ext == ".png" ] || [ $file_ext == ".jpg" ] || [ $file_ext == ".jpg" ] || [ $file_ext == ".gif" ] || [ $file_ext == ".tiff" ] || [ $file_ext == ".psd" ] || [ $file_ext == ".ai" ] || [ $file_ext == ".indd" ] || [ $file_ext == ".raw" ];
+                if [ $file_ext == ".png" ] || [ $file_ext == ".jpg" ] || [ $file_ext == ".jpeg" ];
+                        # || [ $file_ext == ".gif" ] || [ $file_ext == ".tiff" ] || [ $file_ext == ".psd" >
                 then
-			exif $file_name 
-		else
-		echo "file not compatible for EXIF data extraction"
+                        exif $file_name
+                        #echo "Test, extension ok"
+                        echo " "
+                        shift
+                else
+                        echo "File not compatible for EXIF data extraction"
+                        echo " "
+                        shift
                 fi
-        fi 
+        else
+                echo "Warning: Only files are compatible with this module!"
+        fi
 done;
-
 }
 
 
-# function calling
-metadata_Extraction
-
+#function calling
+metadata_Extraction $@
