35880b7bf7199ec6fea6f10e2ef29090ac434a55
SteveSquared14 <89590079+SteveSquared14@users.noreply.github.com>
2022-07-11 11:20:16 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ToolsDirectory=~/ReconTool
+
+sudo apt update
+
+mkdir Findings
+#chown $ToolsDirectory `id -u -n`:`id -g -n`
+
+
+#if [ ! -d $ToolsDirectory ]; then
+#	echo "[!] Directory for tools ($ToolsDirectory) is not accessible or created"
+#	echo "Exiting..."
+#fi
+
+#cd ..
+#mv ReconTool $ToolsDirectory/ReconTool
+#cd $ToolsDirectory
+WorkingDrive="$(pwd)"
+sudo ln -s $WorkingDrive /usr/local/bin/ReconTool
+
+#Install exif for metadata extraction
+#cd ReconTool
+sudo apt install exif
+chmod 555 reconToolV6.sh
+
+echo "Installation finished!..."
+echo "[!] Restart your terminal for changes to take effect"
+echo
