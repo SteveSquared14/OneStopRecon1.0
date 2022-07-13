eec8074ea27228226bbecfc66b63fcb572a18ef4
SteveSquared14 <89590079+SteveSquared14@users.noreply.github.com>
2022-07-13 10:49:26 -0400

    Add files via upload
    Changed name of tool to that chosen.
    Added in status messages for every module.
    Tidied up each file and removed unnecessary comments.
    Finalised and spell checked all files.

--- dorking.sh
+++ dorking.sh
@@ -8,16 +8,16 @@ searchTerm="$2"
 
 #initialise an associative array for different search engine dorking
 declare -A searchEnginesArray
-searchEnginesArray[-dBa]="https://www.baidu.com/s?wd="
-searchEnginesArray[-dBi]="https://www.bing.com/search?q="
+#searchEnginesArray[-dBa]="https://www.baidu.com/s?wd="
+searchEnginesArray[-dB]="https://www.bing.com/search?q="
 searchEnginesArray[-dD]="https://www.duckduckgo.com/?q="
 searchEnginesArray[-dG]="https://www.google.com/search?q="
 
 #determine which search engine is to be used dependant on mode selected by user
 for key in "${!searchEnginesArray[@]}"; do
-	if [[ "$1" == "$key" ]]; then
-		mainUrl="${searchEnginesArray[$key]}"
-	fi
+        if [[ "$1" == "$key" ]]; then
+                mainUrl="${searchEnginesArray[$key]}"
+        fi
 done
 
 
@@ -25,83 +25,96 @@ done
 dorking_cache(){
         append="cache" 
         concatUrl=$mainUrl$append$colonHex$searchTerm
+	echo "[*] Executing cache dork in chosen search engine..."
         xdg-open $concatUrl
 }
 
 dorking_site(){
         append="site"
         concatUrl=$mainUrl$append$colonHex$searchTerm
+	echo "[*] Executing site dork in chosen search engine..."
         xdg-open $concatUrl
 }
 
 dorking_inurl(){
         append="inurl" 
         concatUrl=$mainUrl$append$colonHex$searchTerm
+	echo "[*] Executing InUrl dork in chosen search engine..."
         xdg-open $concatUrl
 }
 
 dorking_intitle(){
         append="intitle" 
         concatUrl=$mainUrl$append$colonHex$searchTerm
+	echo "[*] Executing InTitle dork in chosen search enginer..."
         xdg-open $concatUrl
 }
 
 dorking_intext(){
         append="intext"
         concatUrl=$mainUrl$append$colonHex$searchTerm
+	echo "[*] Executing InText dork in chosen search engine..."
         xdg-open $concatUrl
 }
                                                                                                                                    
                                                                                                                                    
 dorking_fileType_PDF(){                                                                                                                                                                                                            
         urlWithTerm=$mainUrl$searchTerm                                                                                           
-        concatUrl=$urlWithTerm$fileConvention"pdf"                                                                                                                                                                                       
+        concatUrl=$urlWithTerm$fileConvention"pdf"                 
+	echo "[*] Executing PDF file type dork in chosen search engine..."	
         xdg-open $concatUrl                                                                                                        
 }                                                                                                                                  
                                                                                                                                    
 dorking_fileType_PNG(){                                                                                                                                                                                                            
         urlWithTerm=$mainUrl$searchTerm                                                                                           
-        concatUrl=$urlWithTerm$fileConvention"png"                                                                                                           
+        concatUrl=$urlWithTerm$fileConvention"png"                        
+	echo "[*] Executing PNG file type dork in chosen search engine..."	
         xdg-open $concatUrl                                                                                                        
 }                                                                                                                                  
                                                                                                                                    
 dorking_fileType_JPEG(){                                                                                                                                                                                                           
         urlWithTerm=$mainUrl$searchTerm                                                                                           
-        concatUrl=$urlWithTerm$fileConvention"jpeg"                                                                                                                                                                                      
+        concatUrl=$urlWithTerm$fileConvention"jpeg"                       
+	echo "[*] Executing JPEG file type dork in chosen search engine..." 	
         xdg-open $concatUrl                                                                                                        
                                                                                                                                    
 }                                                                                                                                  
                                                                                                                                    
 dorking_fileType_XML(){                                                                                                                                                                                                            
         urlWithTerm=$mainUrl$searchTerm                                                                                           
-        concatUrl=$urlWithTerm$fileConvention"xml"                                                                                                                                                                                        
+        concatUrl=$urlWithTerm$fileConvention"xml"                         
+	echo "[*] Executing XML file type dork in chosen search engine..."	
         xdg-open $concatUrl                                                                                                        
                                                                                                                                    
 }                                                                                                                                  
                                                                                                                                    
 dorking_fileType_LOG(){                                                                                                                                                                                                           
         urlWithTerm=$mainUrl$searchTerm                                                                                           
-        concatUrl=$urlWithTerm$fileConvention"log"                                                                                                                                                                                     
+        concatUrl=$urlWithTerm$fileConvention"log"    
+	echo "[*] Executing LOG file type dork in chosen search engine..."	
         xdg-open $concatUrl                                                                                                        
                                                                                                                                    
 }
 
 dorking_fileType_XLSX(){
-	urlWithTerm=$mainUrl$searchTerm
-	concatUrl=$urlWithTerm$fileConvention"xlsx"
-	xdg-open $concatUrl
+        urlWithTerm=$mainUrl$searchTerm
+        concatUrl=$urlWithTerm$fileConvention"xlsx"
+	echo "[*] Executing XLSX file type dork in chosen search engine..."
+        xdg-open $concatUrl
 }
 
 dorking_fileType_TXT(){
-	urlWithTerm=$mainUrl$searchTerm
-	concatUrl=$urlWithTerm$fileConvention"txt"
-	xdg-open $concatUrl
+        urlWithTerm=$mainUrl$searchTerm
+        concatUrl=$urlWithTerm$fileConvention"txt"
+	echo "[*] Executing TXT file type dork in chosen search engine..."
+        xdg-open $concatUrl
 }
 
 
 Excute_All_Dorkings(){
-	dorking_cache
-	dorking_site
+	echo "[*] Please allow up to 30 seconds for all dorks to execute completely..."
+        dorking_cache
+        dorking_site
         dorking_inurl
         dorking_intitle
         dorking_intext
--- install.sh
+++ install.sh
@@ -10,20 +10,20 @@ sudo ln -s $WorkingDrive /usr/local/bin/ReconTool
 
 #Install exif for metadata extraction
 sudo apt install exif
-chmod 755 reconToolV6.sh
+chmod 755 reconTool.sh
 
-echo "Installation finished!..."
+echo "[*] Installation finished!..."
 echo " "
 echo " "
 #Print out of welcome message
 echo -e "==============================================================================================================================================================================="
-echo -e "" \
-                 "\t\t\t\t\t\t\t\t\t  ____                    _____           _  \n" \
-                 "\t\t\t\t\t\t\t\t\t |  _ \ ___  ___ ___  _ _|_   _|__   ___ | | \n" \
-                 "\t\t\t\t\t\t\t\t\t | |_) / _ \/ __/ _ \| '_ \| |/ _ \ / _ \| | \n" \
-                 "\t\t\t\t\t\t\t\t\t |  _ <  __/ (_| (_) | | | | | (_) | (_) | | \n" \
-                 "\t\t\t\t\t\t\t\t\t |_| \_\___|\___\___/|_| |_|_|\___/ \___/|_| \n" \
-                 ""
+echo -e "" \			         "\t\t\t\t\t\t\t  ___             ____  _              ____                      \n" \
+                                         "\t\t\t\t\t\t\t\t / _ \ _ __   ___/ ___|| |_ ___  _ __ |  _ \ ___  ___ ___  _ __  \n" \
+                                         "\t\t\t\t\t\t\t\t| | | | '_ \ / _ \___ \| __/ _ \| '_ \| |_) / _ \/ __/ _ \| '_ \ \n" \
+                                         "\t\t\t\t\t\t\t\t| |_| | | | |  __/___) | || (_) | |_) |  _ <  __/ (_| (_) | | | |\n" \
+                                         "\t\t\t\t\t\t\t\t \___/|_| |_|\___|____/ \__\___/| .__/|_| \_\___|\___\___/|_| |_|\n" \
+                                         "\t\t\t\t\t\t\t\t                                |_|                    	   \n" \
+""
 echo -e "==============================================================================================================================================================================="
 echo " "
 echo "Welcome to ReconTool! Throughout the design/build phase the aim of the game was try and automate a lot of the common/tedious parts of reconnaissance."
@@ -34,16 +34,17 @@ echo " "
 echo "Disclaimer: While a lot of the modules are Passive in nature, there is one that could be considered Semi-Passive/Active so please only use with caution, or" 
 echo "necessary authorisation."
 echo " "
-echo "==================================================================================== Modules: =================================================================================="
+echo "==================================================================================== Modules: ================================================================================="
 echo -e "\n"
        
-        echo -e " -dB | --Dorking \t\t\tRun popular dorking searches using Bing search engine \t\t| Takes desired search term as an argument\t\t[Passive]"
+        echo -e " -b  | --Banner Grabbing \t\tGain details about a system on a network \t\t\t| Takes a domain as an argument\t\t\t\t[Semi-Passive]"
+	echo -e " -dB | --Dorking \t\t\tRun popular dorking searches using Bing search engine \t\t| Takes desired search term as an argument\t\t[Passive]"
         echo -e " -dD | --Dorking \t\t\tRun popular dorking searches using Duck Duck Go search engine \t| Takes a desired search term as an argument\t\t[Passive]"
         echo -e " -dG | --Dorking \t\t\tRun popular dorking searches using Google search engine \t| Takes a desried search term as an arugment\t\t[Passive]"
-        echo -e " -g  | --grabbing banner \t\tGain deatils about a system on a network \t\t\t| Takes a domain as an argument\t\t\t\t[Passive]"
 	echo -e " -M  | --MetaDataExtractor\t\tExracts data from files \t\t\t\t\t| File(s) to be analysed to be passed as parameters\t[Passive]"
         echo -e " -m  | --Google Map Request\t\tOpens google map to inputted location \t\t\t\t| Address to be located to be passed as argument\t[Passive]"
-        echo -e " -n  | --Name Server Enumeration \tLocating all DNS servers and records for a company \t\t| Takes a domain as an argument\t\t\t\t[Passive/Active]"
+        echo -e " -n  | --Name Server Enumeration \tLocating all DNS servers and records for a company \t\t| Takes a domain as an argument\t\t\t\t[Semi-Passive]"
         echo -e " -s  | --Shodan  \t\t\tIoT devices check \t\t\t\t\t\t| Takes desired search term as an arugment\t\t[Passive]"
         echo -e " -t  | --Text file recon \t\tChecks for intel in Robots.txt & Security.txt of domain \t| Takes a domain as an argument\t\t\t\t[Passive]"
         echo -e " -w  | --Whois \t\t\t\tWhois URL check \t\t\t\t\t\t| Takes a domain as an arugment\t\t\t\t[Passive]"
+
